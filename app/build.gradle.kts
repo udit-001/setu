7713db936dee8b514dc6351ac1402420347269ea
Udit <23015406+udit-001@users.noreply.github.com>
2026-07-27 00:38:59 +0530

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "ai.uktam"
         minSdk = 34
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 10
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
