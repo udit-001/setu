b95e593839efc320ce95119265bf1978ee0d7acc
Udit <23015406+udit-001@users.noreply.github.com>
2026-09-25 20:50:35 +0530

    Bump version to 1.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ai.setu"
         minSdk = 31
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.1.3"
+        versionCode = 17
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
